fc9060722d214ba2cedc8b3be8b4f4481c42497b
ranran <931094193@qq.com>
2017-05-11 22:29:47 +0800

    .gitignore

---
+++ src/First/Singleton.java
@@ -0,0 +1,12 @@
+package First;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package First
+ * @Description:
+ * @date 2017/5/11 21:57
+ */
+public class Singleton5 {
+}
---
+++ src/First/Singleton2.java
@@ -0,0 +1,12 @@
+package First;
+
+/**
+ * @author ranran
+ * @version V1.0
+ * @Title:
+ * @Package First
+ * @Description:
+ * @date 2017/5/11 22:11
+ */
+public class Singleton2 {
+}
