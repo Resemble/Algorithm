b4aec2b46fd5fa0ac67cc1c155701a9ef625f25b
ranran <931094193@qq.com>
2017-07-21 14:45:11 +0800

    BubbleSort

--- src/Sort/BubbleSortTest.java
+++ src/Sort/BubbleSortTest.java
@@ -5,8 +5,38 @@ package Sort;
  * @version V1.0
  * @Title:
  * @Package Sort
- * @Description:
+ * @Description: 冒泡排序
  * @date 2017/7/21 14:33
  */
 public class BubbleSortTest {
+
+    public static void BublleSort(int array[]) {
+        int i, j;
+        int len = array.length;
+        int tmp;
+        for (i = 0; i < len - 1; i++) {
+            for (j = len - 1; j > i; j--) {
+                if (array[j] < array[j-1]) {
+                    tmp = array[j];
+                    array[j] = array[j-1];
+                    array[j-1] = tmp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        long startTime;
+        long endTime;
+        startTime = System.nanoTime();
+        int array[] = {3, 34, 43, 2, 543, 23, 13, 324};
+        BublleSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+        endTime = System.nanoTime();
+        System.out.println(String.format("this take %s ns", (endTime - startTime)));
+    }
+
+
 }
