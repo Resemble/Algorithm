ed9a5692f5d4ea6ccf8e5046379f22a99856391f
ranran <931094193@qq.com>
2017-07-21 16:57:25 +0800

    Shell Sort

--- src/Sort/ShellSortTest.java
+++ src/Sort/ShellSortTest.java
@@ -9,4 +9,25 @@ package Sort;
  * @date 2017/7/21 16:32
  */
 public class ShellSortTest {
+
+    public static void shellSort(int array[]) {
+        int length = array.length;
+        int i, j;
+        int h;
+        int temp;
+        for (h = length / 2; h > 0; h = h / 2) {
+            for (i = h; i < length; i++) {
+                temp = array[i];
+                for (j = i - h; j >= 0; j -= h) {
+                    if (temp < array[j]) {
+                        array[j + h] = array[j];
+                    } else {
+                        break;
+                    }
+                }
+                array[j + h] = temp;
+            }
+        }
+    }
+
 }
