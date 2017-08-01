e8d98b274c99774b5593c78b68c3f5995055044e
ranran <931094193@qq.com>
2017-08-01 10:15:49 +0800

    链表倒数第 n 个

--- src/First/NthToLastElementSolution.java
+++ src/First/NthToLastElementSolution.java
@@ -5,8 +5,34 @@ package First;
  * @version V1.0
  * @Title:
  * @Package First
- * @Description:
+ * @Description: 找到单链表倒数第n个节点，保证链表中节点的最少数量为n。
  * @date 2017/8/1 9:08
  */
 public class NthToLastElementSolution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    ListNode nthToLast(ListNode head, int n) {
+        // write your code here
+        ListNode fastNode = head;
+        ListNode slowNode = head;
+        for (int i = 0; i < n; i++) {
+            fastNode = fastNode.next;
+        }
+        while(fastNode != null) {
+            slowNode = slowNode.next;
+            fastNode = fastNode.next;
+        }
+        return slowNode;
+
+    }
+
+
 }
