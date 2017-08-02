46db9d777e0dc74ef34d6778ddc1a2b70fb4f311
ranran <931094193@qq.com>
2017-08-02 17:30:42 +0800

    归并排序,时间复杂度低，而且好看

--- src/Sort/MergeSortTest2.java
+++ src/Sort/MergeSortTest2.java
@@ -1,69 +1,66 @@
 package Sort;
 
 
-import static Sort.QuickSortTest2.getRandomArray;
-
 /**
  * @author ranran
  * @version V1.0
  * @Title:
  * @Package Sort
- * @Description:
+ * @Description: 时间复杂度低，而且好看
  * @date 2017/7/30 21:31
  */
 public class MergeSortTest2 {
-    //                                         p        q        r
-    public static void Merge(int array[], int low, int mid, int high) {
-        int i, j, k, n1, n2;
-        n1 = mid - low + 1;
-        n2 = high - mid;
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-        for (i = 0, k = low; i < n1; i++, k++) {
-            L[i] = array[k];
+
+
+    public static void sort(int[] arr) {
+        int[] helper = new int[arr.length];
+        sort(arr, 0, arr.length - 1, helper);
+    }
+
+    private static void sort(int[] arr, int low, int high, int[] helper) {
+        if (low >= high) {
+            return;
         }
-        for (i = 0, k = mid + 1; i < n2; i++, k++) {
-            R[i] = array[k];
+        int mid = low + (high - low) / 2;
+        sort(arr, low, mid, helper);
+        sort(arr, mid + 1, high, helper);
+        merge(arr, low, mid, high, helper);
+    }
+
+    private static void merge(int[] arr, int low, int mid, int high, int[] helper) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = arr[i];
         }
-        for (k = low, i = 0, j = 0; i < n1 && j < n2; k++) {
-            // 2个都遍历完，排序
-            if (L[i] > R[j]) {
-                array[k] = L[i];
-                i++;
+        int i = low;
+        int j = mid + 1;
+        for (int k = low; k <= high; k++) {
+            if (i > mid) {
+                // 左边已合并完
+                arr[k] = helper[j++];
+            } else if (j > high) {
+                // 右边已合并完
+                arr[k] = helper[i++];
+            } else if (helper[i] < helper[j]) {
+                // 左边小于右边
+                arr[k] = helper[i++];
             } else {
-                array[k] = R[j];
-                j++;
-            }
-        }
-        if (i < n1) {
-            for (j = i; j < n1; j++, k++) {
-                array[k] = L[j];
-            }
-        }
-        if (j < n2) {
-            for (i = j; i < n2; i++, k++) {
-                array[k] = R[i];
+                // 左边大于等于右边
+                arr[k] = helper[j++];
             }
 
         }
+
     }
 
-
-    public static void MergeSort(int array[], int low, int high) {
-        if (low < high) {
-            int mid = (low + high) / 2;
-            MergeSort(array, low, mid);
-            MergeSort(array, mid + 1, high);
-            Merge(array, low, mid, high);
-        }
-    }
 
     public static void main(String[] args) {
         long startTime;
         long endTime;
         startTime = System.nanoTime();
-        int array[] = getRandomArray(400);
-        MergeSort(array, 0, array.length-1);
+//        int array[] = getRandomArray(400);
+        int array[] = {3,2,1,4,5};
+        sort(array);
+
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
