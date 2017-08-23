5d38a2671d27ff8c36b713263952f7fba3e1a670
ranran <931094193@qq.com>
2017-08-23 22:29:59 +0800

    test

--- src/Third/Test.java
+++ src/Third/Test.java
@@ -9,4 +9,15 @@ package Third;
  * @date 2017/8/23 20:20
  */
 public class Test {
+
+    public static void main(String[] args) {
+        String string = "string";
+        for (int i = 0; i < string.length(); i++) {
+            System.out.println(string.charAt(i));
+        }
+        System.out.println(string.substring(2));
+        System.out.println(string.substring(2,5));
+        System.out.println(string.substring(2,6));
+    }
+
 }
