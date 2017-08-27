b046885e647774efa1d548e96c71c81eb31f6cab
ranran <931094193@qq.com>
2017-08-27 11:56:42 +0800

    滴滴

--- src/Third/GetTheNthNumber2.java
+++ src/Third/GetTheNthNumber2.java
@@ -2,6 +2,7 @@ package Third;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * @author ranran
@@ -11,52 +12,81 @@ import java.util.Scanner;
  * @Description:
  * @date 2017/8/26 16:02
  */
-public class GetTheNthNumber {
+public class GetTheNthNumber2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Integer> arrayList = new ArrayList<>();
-        while(scanner.hasNext()) {
-            arrayList.add(scanner.nextInt());
-        }
+//        while(scanner.hasNext()) {
+//            arrayList.add(scanner.nextInt());
+//        }
+        arrayList.add(45);
+        arrayList.add(55);
+        arrayList.add(67);
+        arrayList.add(33);
+        arrayList.add(57);
+        arrayList.add(4);
 
         int Nth = arrayList.get(arrayList.size() - 1);
+        Stack<Integer> stack = new Stack<>();
+//        stack.push(34);
+//        stack.push(35);
+//        System.out.println(stack.firstElement());
+//        System.out.println(stack.get(0));
+//        System.out.println(stack.get(1));
+
+
         int[] array = new int[arrayList.size() - 1];
         for (int i = 0; i < array.length; i++) {
             array[i] = arrayList.get(i);
-        }
-
-        quickSort(array);
-        System.out.println(array[array.length - Nth]);
-
-    }
-
-
-    public static void sort(int array[], int low, int high) {
-        if (low > high) {
-            return;
-        }
-        int i = low;
-        int j = high;
-        int temp = array[i];
-        while (i < j) {  // while 循环保证一个数一直填坑
-            while (i < j && array[j] >= temp) {
-                j--;
+            if (i < Nth) {
+                adjustStack2(stack, arrayList.get(i));
+            } else {
+                adjustStack(stack, arrayList.get(i), Nth);
             }
-            array[i] = array[j];
-            while (i < j && array[i] < temp) {
-                i++;
-            }
-            array[j] = array[i];
         }
-        array[j] = temp;
-        sort(array, low, j - 1);
-        sort(array, j + 1, high);
+
+        System.out.println(stack.peek());
+
     }
 
-    public static void quickSort(int array[]) {
-        sort(array, 0, array.length - 1);
+    public static void adjustStack(Stack<Integer> stack, int value, int Nth) {
+        Stack<Integer> tempStack = new Stack<>();
+        int i = 0;
+        while (!stack.isEmpty()) {
+            if (stack.peek() < value) {
+                if (i == 0) {
+                    stack.pop();
+                    i++;
+                } else {
+                    tempStack.push(stack.pop());
+                }
+            } else {
+                break;
+            }
+        }
+        if (stack.size() < Nth) {
+            stack.push(value);
+        }
+
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
     }
 
+    public static void adjustStack2(Stack<Integer> stack, int value) {
+        Stack<Integer> tempStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            if (stack.peek() < value) {
+                tempStack.push(stack.pop());
+            } else {
+                break;
+            }
+        }
+        stack.push(value);
+        while (!tempStack.isEmpty()) {
+            stack.push(tempStack.pop());
+        }
+    }
 
 }
--- src/Third/SubString.java
+++ src/Third/SubString.java
@@ -1,5 +1,8 @@
 package Third;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * @author ranran
  * @version V1.0
@@ -9,4 +12,29 @@ package Third;
  * @date 2017/8/26 15:43
  */
 public class SubString {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        while(scanner.hasNext()) {
+            arrayList.add(scanner.nextInt());
+        }
+//        arrayList.add(-23);
+//        arrayList.add(17);
+//        arrayList.add(-7);
+//        arrayList.add(11);
+//        arrayList.add(-2);
+//        arrayList.add(1);
+//        arrayList.add(-34);
+
+        int max = Integer.MIN_VALUE;
+        int tempMax = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            tempMax = Math.max(arrayList.get(i), tempMax + arrayList.get(i));
+            max = Math.max(max, tempMax);
+        }
+        System.out.println(max);
+    }
+
+
 }
