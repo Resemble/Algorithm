3da148a0d0ea9c7f0192dc5b2fabbe3d83cf770c
ranran <931094193@qq.com>
2017-08-31 11:06:20 +0800

    modify

--- src/Second/BinTree.java
+++ src/Second/BinTree.java
@@ -106,18 +106,25 @@ public class BinTree {
         TreeNode node3 = new TreeNode(3);
         TreeNode node4 = new TreeNode(4);
         TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
         /**
          *      1
          *    /   \
          *   2     3
          *  /       \
          * 4         5
+          / \
+         7   6
          */
+
 
         node1.left = node2;
         node1.right = node3;
         node2.left = node4;
         node3.right = node5;
+        node4.right = node6;
+        node4.left = node7;
         System.out.println();
         preOrder(node1);
         System.out.println();
