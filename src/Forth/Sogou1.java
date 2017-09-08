c5d8cdc2d98900d05e1d1c5d3703407d5b266c8d
ranran <931094193@qq.com>
2017-09-08 22:50:25 +0800

    printf %.8f,d

--- src/Forth/Sogou1.java
+++ src/Forth/Sogou1.java
@@ -9,4 +9,10 @@ package Forth;
  * @date 2017/9/8 16:57
  */
 public class Sogou1 {
+
+    public static void main(String[] args) {
+        double des = 2.0;
+        System.out.printf("%.8f", des);
+    }
+
 }
