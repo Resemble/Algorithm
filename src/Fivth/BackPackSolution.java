f2cd822a93389950eea3c1822da274f2634f9b66
ranran <931094193@qq.com>
2017-09-12 22:02:37 +0800

    背包问题

--- src/Fivth/BackPackSolution.java
+++ src/Fivth/BackPackSolution.java
@@ -5,8 +5,32 @@ package Fivth;
  * @version V1.0
  * @Title:
  * @Package Fivth
- * @Description:
+ * @Description: 背包问题
+ * 在n个物品中挑选若干物品装入背包，最多能装多满？假设背包的大小为m，每个物品的大小为A[i]
+ * 注意事项
+ * 你不可以将物品进行切割。
+ * 您在真实的面试中是否遇到过这个题？ Yes
+ * 样例
+ * 如果有4个物品[2, 3, 5, 7]
+ * 如果背包的大小为11，可以选择[2, 3, 5]装入背包，最多可以装满10的空间。
+ * 如果背包的大小为12，可以选择[2, 3, 7]装入背包，最多可以装满12的空间。
+ * 函数需要返回最多能装满的空间大小。
  * @date 2017/9/11 17:33
  */
 public class BackPackSolution {
+
+    public int backPack(int m, int[] A) {
+        // write your code here
+        int[][] dp = new int[A.length + 1][m + 1];
+        for (int i = 1; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (j >= A[i - 1]) {
+                    dp[i][j] = dp[i - 1][j - A[i - 1]] + A[i - 1];
+                }
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+            }
+        }
+        return dp[A.length][m];
+    }
+
 }
--- src/Fivth/BackPackVISolution.java
+++ src/Fivth/BackPackVISolution.java
@@ -5,8 +5,39 @@ package Fivth;
  * @version V1.0
  * @Title:
  * @Package Fivth
- * @Description:
+ * @Description: 背包问题 VI
+ * 给出一个都是正整数的数组 nums，其中没有重复的数。从中找出所有的和为 target 的组合个数。
+ * 注意事项
+ * 一个数可以在组合中出现多次。
+ * 数的顺序不同则会被认为是不同的组合。
+ * 您在真实的面试中是否遇到过这个题？ Yes
+ * 样例
+ * 给出 nums = [1, 2, 4], target = 4
+ * 可能的所有组合有：
+ * [1, 1, 1, 1]
+ * [1, 1, 2]
+ * [1, 2, 1]
+ * [2, 1, 1]
+ * [2, 2]
+ * [4]
+ * 返回 6
  * @date 2017/9/11 17:12
  */
 public class BackPackVISolution {
+
+    public int backPackVI(int[] nums, int target) {
+        // write your code here
+
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] <= i) {
+                    dp[i] += dp[i - nums[j]];
+                }
+            }
+        }
+        return dp[target];
+    }
+
 }
