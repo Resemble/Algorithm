5c748cd3f17263b8f6ca77ded8535117bb71527a
ranran <931094193@qq.com>
2017-09-13 22:42:14 +0800

    最长公共前缀

--- src/Fivth/LongestCommonPrefixSolution.java
+++ src/Fivth/LongestCommonPrefixSolution.java
@@ -5,8 +5,47 @@ package Fivth;
  * @version V1.0
  * @Title:
  * @Package Fivth
- * @Description:
+ * @Description: 最长公共前缀
  * @date 2017/9/13 14:35
  */
 public class LongestCommonPrefixSolution {
+
+    public static void main(String[] args) {
+        String[] strs = new String[2];
+        strs[0] = "A";
+        strs[1] = "A";
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+
+    public static String longestCommonPrefix(String[] strs) {
+        // write your code here
+        Character ch;
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+        int minLen = Integer.MAX_VALUE;
+        int index = 0;
+        for(int i = 0; i < strs.length; i++) {
+            if (minLen > strs[i].length()) {
+                minLen = strs[i].length();
+                index = i;
+            }
+
+        }
+        for(int j = 0; j < minLen; j++) {
+            ch = strs[0].charAt(j);
+            for(int i = 0; i < strs.length; i++) {
+                if (ch != strs[i].charAt(j)) {
+                    if (j >= 1) {
+                        return strs[0].substring(0, j);
+                    }else {
+                        return "";
+                    }
+                }
+            }
+        }
+
+        return minLen == 0 ? "":strs[index];
+    }
+
 }
