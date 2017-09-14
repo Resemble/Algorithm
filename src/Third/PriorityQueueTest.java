60d63e88f657e3733fc2be4daec784bc1857775e
ranran <931094193@qq.com>
2017-09-14 22:21:04 +0800

    modify

--- src/Third/PriorityQueueTest.java
+++ src/Third/PriorityQueueTest.java
@@ -1,6 +1,8 @@
 package Third;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
+import java.util.Random;
 
 /**
  * @author ranran
@@ -12,15 +14,39 @@ import java.util.PriorityQueue;
  */
 public class PriorityQueueTest {
     public static void main(String[] args) {
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();  // 默认小堆 获取TopN大
+//        PriorityQueue<Integer> priorityQueue = getTopNSamll();  // 使用 comparator 获取TopN小
         priorityQueue.add(4);
         priorityQueue.add(2);
         priorityQueue.add(9);
         priorityQueue.add(5);
         priorityQueue.add(22);
         priorityQueue.add(11);
+        Random random = new Random(1000);
+        for (int i = 0; i < 100; i++) {
+            if (priorityQueue.size() > 10) {
+                priorityQueue.poll();
+            }
+            priorityQueue.add(random.nextInt(100));
+        }
         while(!priorityQueue.isEmpty()) {
             System.out.println(priorityQueue.poll());
         }
     }
+
+
+
+    // 模拟大顶堆 解决 TopN小问题
+    public static PriorityQueue<Integer> getTopNSamll() {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        return priorityQueue;
+
+    }
+
+
 }
