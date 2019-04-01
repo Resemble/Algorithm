6bd45572b4a377788c42e39ca840fa6b9fa6be7b
ranbo02 <ranbo02@meituan.com>
2019-04-01 22:31:35 +0800

    队列用两个栈实现

--- src/eighth/MyQueue.java
+++ src/eighth/MyQueue.java
@@ -1,5 +1,7 @@
 package eighth;
 
+import java.util.Stack;
+
 /**
  * @author ranbo
  * @version V1.0
@@ -9,4 +11,57 @@ package eighth;
  * @date 2019/3/31 10:10 AM
  */
 public class MyQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    public MyQueue() {
+        // do intialization if necessary
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /*
+     * @param element: An integer
+     * @return: nothing
+     */
+    public void push(int element) {
+        // write your code here
+        while (!stack2.empty()) {
+            stack1.push(stack2.pop());
+        }
+        stack1.push(element);
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        while (!stack1.empty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int top() {
+        // write your code here
+        while (!stack1.empty()) {
+            stack2.push(stack1.pop());
+        }
+        return stack2.peek();
+    }
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1);
+        myQueue.pop();
+        myQueue.push(2);
+        myQueue.push(3);
+        myQueue.top();
+        myQueue.pop();
+    }
 }
