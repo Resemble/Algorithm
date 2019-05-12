aa154085fb6e2bc84ea46da9d912d8c1b7ba2e60
ranbo02 <ranbo02@meituan.com>
2019-05-12 11:11:56 +0800

    简单题

--- src/ninth/FizzBuzzSolution.java
+++ src/ninth/FizzBuzzSolution.java
@@ -1,5 +1,8 @@
 package ninth;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author ranbo
  * @version V1.0
@@ -9,4 +12,23 @@ package ninth;
  * @date 2019-05-09 23:15
  */
 public class FizzBuzzSolution {
+
+
+    /**
+     * @param n: An integer
+     * @return: A list of strings.
+     */
+    public List<String> fizzBuzz(int n) {
+        // write your code here
+        List<String> result = new ArrayList<>();
+
+        String temp;
+        for (int i = 1; i < n + 1; i++) {
+            temp = String.valueOf(i);
+            temp = (i % 3 == 0 && i % 5 == 0) ? "fizz buzz" : (i % 3 == 0) ? "fizz" : (i % 5 == 0) ? "buzz" : temp;
+            result.add(temp);
+        }
+        return result;
+    }
+
 }
