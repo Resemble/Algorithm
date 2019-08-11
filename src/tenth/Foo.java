73bb0d86261ac271e0c5eb434d255abeacf04425
ranbo02 <ranbo02@meituan.com>
2019-08-11 16:36:10 +0800

    按序打印

---
+++ src/tenth/Foo.java
@@ -0,0 +1,41 @@
+package tenth;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author ranbo
+ * @version V1.0
+ * @Title:
+ * @Package tenth
+ * @Description:
+ * @date 2019-08-11 16:18
+ */
+public class Foo {
+
+    private CountDownLatch second = new CountDownLatch(1);
+    private CountDownLatch third = new CountDownLatch(1);
+
+    public Foo() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        second.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        second.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        third.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        third.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
