c353307702db9fbae84dacb95c61437d465e6c5f
ranbo02 <ranbo02@meituan.com>
2020-03-01 17:07:42 +0800

    合并两个有序数组

--- src/tenth/MergeSortArray.java
+++ src/tenth/MergeSortArray.java
@@ -5,8 +5,23 @@ package tenth;
  * @version V1.0
  * @Title:
  * @Package tenth
- * @Description:
+ * @Description: 合并两个有序数组
  * @date 2020-03-01 16:53
  */
 public class MergeSortArray {
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int j = m - 1;
+        int k = n - 1;
+        for (int i = m + n - 1; i >= 0; i--) {
+            if ((j >= 0 && k >= 0 && nums1[j] >= nums2[k]) || (j >= 0 && k < 0)) {
+                nums1[i] = nums1[j];
+                j--;
+            } else if (k >= 0){
+                nums1[i] = nums2[k];
+                k--;
+            }
+        }
+    }
+
 }
