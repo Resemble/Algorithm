f2abfe51b3b55a2ef895d662fe84789f47805ee6
ranbo02 <ranbo02@meituan.com>
2020-06-27 17:59:28 +0800

    5. 最长回文子串

---
+++ src/package11/LongestPalindromeSolution.java
@@ -0,0 +1,48 @@
+package package11;
+
+/**
+ * @author ranbo
+ * @version V1.0
+ * @Title:
+ * @Package package11
+ * @Description: 5. 最长回文子串
+ * https://leetcode-cn.com/problems/longest-palindromic-substring/
+ * @date 2020-06-21 18:48
+ */
+public class LongestPalindromeSolution {
+
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        // dp[i][j] 表示 s[i, j] 是否是回文串
+        boolean[][] dp = new boolean[len][len];
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int j = 1; j < len; j++) {
+            for (int i = 0; i < j; i++) {
+                if (charArray[i] != charArray[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                    if (dp[i][j] && j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        begin = i;
+                    }
+                }
+            }
+
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+}
