235dc47a47babb494edb772e1b9a8d5add51ff80
ranbo02 <ranbo02@meituan.com>
2020-07-04 15:40:19 +0800

    Fizz Buzz

---
+++ src/package11/FizzBuzzSolution.java
@@ -0,0 +1,32 @@
+package package11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ranbo
+ * @version V1.0
+ * @Title:
+ * @Package package11
+ * @Description: Fizz Buzz
+ * @date 2020-07-04 15:35
+ */
+public class FizzBuzzSolution {
+
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add("" + i);
+            }
+        }
+        return list;
+    }
+
+}
